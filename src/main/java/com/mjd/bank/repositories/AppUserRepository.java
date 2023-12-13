e2a4ab7f2c3e9ee7a6c8db46a171c0b111e6fb20
Juan Pablo López <juanpablo.lopez@mercadolibre.com.co>
2023-12-13 09:18:21 -0500

    Added @Repository annotations and update repository methods

--- src/main/java/com/mjd/bank/repositories/AppUserRepository.java
+++ src/main/java/com/mjd/bank/repositories/AppUserRepository.java
@@ -2,8 +2,9 @@ package com.mjd.bank.repositories;
 
 import com.mjd.bank.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
   AppUser findByEmail(String email);
-  AppUser findAllByCity(String city);
 }
--- src/main/java/com/mjd/bank/repositories/PocketRepository.java
+++ src/main/java/com/mjd/bank/repositories/PocketRepository.java
@@ -3,7 +3,9 @@ package com.mjd.bank.repositories;
 import com.mjd.bank.entities.Pocket;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PocketRepository extends JpaRepository<Pocket, Long> {
   List<Pocket> findAllByAccount_Id(Long accountId);
   List<Pocket> findAllByAccount_Owner_Id(Long ownerId);
