dd045dd7727ed9e19705607da3cd6b73090b0d13
Juan Pablo López <juanpablo.lopez@mercadolibre.com.co>
2023-12-18 19:55:31 -0500

    Add pockets to account detail

--- src/main/java/com/mjd/bank/dtos/response/AccountDetailDTO.java
+++ src/main/java/com/mjd/bank/dtos/response/AccountDetailDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.mjd.bank.entities.AccountType;
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * DTO containing the details of an account
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
  * @param ownerName The name of the owner of the account
  * @param ownerEmail The email of the owner of the account
  * @param balance The amount of money in the account
+ * @param pockets A list containing the pockets of the account - see {@link PocketDetailDTO}
  */
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record AccountDetailDTO(Long accountNumber, AccountType accountType, String ownerName, String ownerEmail, BigDecimal balance) { }
+public record AccountDetailDTO(Long accountNumber, AccountType accountType, String ownerName, String ownerEmail, BigDecimal balance, List<PocketDetailDTO> pockets) { }
--- src/main/java/com/mjd/bank/utils/mappers/AccountMapper.java
+++ src/main/java/com/mjd/bank/utils/mappers/AccountMapper.java
@@ -1,7 +1,10 @@
 package com.mjd.bank.utils.mappers;
 
 import com.mjd.bank.dtos.response.AccountDetailDTO;
+import com.mjd.bank.dtos.response.PocketDetailDTO;
 import com.mjd.bank.entities.Account;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -9,7 +12,12 @@ public class AccountMapper {
 
   public AccountDetailDTO toAccountDetailDTO(Account account) {
     String completeName = String.format("%s %s", account.getOwner().getName(), account.getOwner().getLastName());
-    return new AccountDetailDTO(account.getNumber(), account.getType(), completeName, account.getOwner().getEmail(), account.getBalance());
+
+    List<PocketDetailDTO> pockets = account.getPockets().stream()
+        .map(pocket -> new PocketDetailDTO(pocket.getId(), pocket.getName(), pocket.getBalance()))
+        .toList();
+
+    return new AccountDetailDTO(account.getNumber(), account.getType(), completeName, account.getOwner().getEmail(), account.getBalance(), pockets);
   }
 
 }
